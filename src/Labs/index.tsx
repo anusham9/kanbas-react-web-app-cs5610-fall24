0be0b2da6bef853f0e01c950aa3bb34f32b52076
anusham9 <mannava.anusha18@gmail.com>
2024-09-14 21:54:09 -0700

    a1 lab

---
+++ src/Labs/index.tsx
@@ -0,0 +1,12 @@
+import Labs1 from './Lab1';
+import Labs2 from './Labs2';
+
+export default function Labs() {
+  return (
+    <div>
+      <h1>Labs</h1>
+      <Labs1 />
+      <Labs2 />
+    </div>
+  );
+}
