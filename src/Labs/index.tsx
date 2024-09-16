327ae9e6a6cbc317f500daf95d7619faed991584
anusham9 <mannava.anusha18@gmail.com>
2024-09-15 21:02:27 -0700

    getting started with kanbas

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router';
 import Labs1 from './Lab1';
 import Labs2 from './Labs2';
+import TOC from './TOC';
 
 export default function Labs() {
   return (
     <div>
+      <h1>Anusha Mannava</h1>
+      <TOC />
       <h1>Labs</h1>
-      <Labs1 />
-      <Labs2 />
+      <Routes>
+        <Route path="Lab1" element={<Labs1 />} />
+        <Route path="Lab2" element={<Labs2 />} />
+        <Route path="Lab3" element={<h2>Lab 3</h2>} />
+      </Routes>
     </div>
   );
 }
