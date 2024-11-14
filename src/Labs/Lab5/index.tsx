c1c97d6981260c9ac0aef3d5de93c01c79da2e2d
anusham9 <mannava.anusha18@gmail.com>
2024-11-13 22:06:08 -0800

    a5 continued

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,8 +1,11 @@
 import EnvironmentVariables from './EnvironmentVariables';
+import HttpClient from './HttpClient';
 import PathParameters from './PathParameters';
 import QueryParameters from './QueryParameters';
 import WorkingWithArrays from './WorkingWithArrays';
+import WorkingWithArraysAsynchronously from './WorkingWithArraysAsynchronously';
 import WorkingWithObjects from './WorkingWithObjects';
+import WorkingWithObjectsAsynchronously from './WorkingWithObjectsAsynchronously';
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
   return (
@@ -23,6 +26,15 @@ export default function Lab5() {
       <WorkingWithObjects />
       <hr />
       <WorkingWithArrays />
+      <hr />
+      <HttpClient />
+      <WorkingWithObjectsAsynchronously />
+      <WorkingWithArraysAsynchronously />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
