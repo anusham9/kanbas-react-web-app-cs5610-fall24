2785d3a7580a64f5fdcce5e0e771a9604cabdeba
anusham9 <mannava.anusha18@gmail.com>
2024-11-23 22:46:44 -0800

    add filtering for users

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,31 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ['Profile'] : ['Signin', 'Signup'];
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? 'active' : '');
+
   return (
     <div
       id="wd-account-navigation"
-      className="wd list-group fs-5 rounded-0 d-none d-md-block"
+      className="wd list-group border-0 rounded-0"
     >
-      <Link
-        className="list-group-item active border border-0"
-        to={`/Kanbas/Account/Signin`}
-      >
-        {' '}
-        Signin{' '}
-      </Link>{' '}
-      <Link
-        className="list-group-item text-danger border border-0"
-        to={`/Kanbas/Account/Signup`}
-      >
-        {' '}
-        Signup{' '}
-      </Link>{' '}
-      <Link
-        className="list-group-item text-danger border border-0"
-        to={`/Kanbas/Account/Profile`}
-      >
-        {' '}
-        Profile{' '}
-      </Link>{' '}
+      {links.map((link: string) => (
+        <Link
+          to={`/Kanbas/Account/${link}`}
+          className={`list-group-item text-danger border border-0 ${
+            pathname.includes(link) ? 'active' : ''
+          }`}
+        >
+          {' '}
+          {link}{' '}
+        </Link>
+      ))}
+      {currentUser && currentUser.role === 'ADMIN' && (
+        <Link
+          to={`/Kanbas/Account/Users`}
+          className={`list-group-item boder-0 border-white ${active('Users')}`}
+        >
+          {' '}
+          Users{' '}
+        </Link>
+      )}
+
+      {/* <Link to={`/Kanbas/Account/Signin`}> Signin </Link> <br />
+      <Link to={`/Kanbas/Account/Signup`}> Signup </Link> <br />
+      <Link to={`/Kanbas/Account/Profile`}> Profile </Link> <br /> */}
     </div>
   );
 }
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -8,6 +8,15 @@ export const findAllUsers = async () => {
   const response = await axiosWithCredentials.get(USERS_API);
   return response.data;
 };
+export const findUsersByRole = async (role: string) => {
+  const response = await axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
 
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(
